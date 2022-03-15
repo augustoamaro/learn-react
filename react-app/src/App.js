d31bd70955e6da9fc94e3ef080e94581529da20b
Augusto <augustoamaro@icloud.com>
2022-03-15 09:24:10 -0300

    initial commit

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Leggo
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
