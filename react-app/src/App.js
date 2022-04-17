d7a9f723233051e7d0e218ec066bf9cde28a55b2
Augusto <augustoamaro@icloud.com>
2022-04-17 17:08:25 -0300

    Added components examples

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,19 @@
-import ExpenseItem from "./components/ExpenseItem";
+import React from "react";
 
-function App() {
+import ExpenseItem from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    { id: 'e1', title: 'Macbook Pro 13', amount: 2000, date: new Date(2021, 5, 12) },
+    { id: 'e2', title: 'Monitor LG 34', amount: 499, date: new Date(2021, 5, 12) },
+    { id: 'e3', title: 'TV Samsung 60', amount: 600, date: new Date(2021, 5, 12) },
+    { id: 'e4', title: 'Sofá', amount: 399, date: new Date(2021, 5, 12) }
+  ];
+
   return (
     <div>
       <h1>Leggo</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem items={expenses} />
     </div>
   );
 }
